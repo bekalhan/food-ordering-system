fcdfce89467446652a158602bb9c3c849d69d0d9
Berat Kalhan <beratkalhan@Berats-MacBook-Air.local>
2024-12-14 08:35:47 -0800

    microservice design pattern

--- infrastructure/saga/src/main/java/com/food/ordering/system/saga/SagaStep.java
+++ infrastructure/saga/src/main/java/com/food/ordering/system/saga/SagaStep.java
@@ -1,5 +1,7 @@
 package com.food.ordering.system.saga;
 
+//Saga dağıtılmış sistem yönetimi için bir tasarımdır
+//Eğer saga stepindebir adım başarısız olursa işlem geri alınır(rollback)
 public interface SagaStep<T> {
     void process(T data);
     void rollback(T data);
